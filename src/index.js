2a6c5bc5eba9db9c8210861e74bb81f954160cba
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-23 02:46:46 +0300

    HashRouter for github

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './components/app/app';
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        {/*BrowserRouter without HashRouter*/}
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
