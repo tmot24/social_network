93520d52a71b879e8ad235bd5cd104bc33941171
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-04-25 21:09:49 +0300

    navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -6,22 +6,22 @@ export const NavBar = () => {
     return (
         <>
             <List component="nav" className={style.nav} disablePadding>
-                <NavLink to={"/profile"} className={style.link} activeClassName={style.activeLink}>
-                    <ListItem button divider>
+                <NavLink to={"/profile"} className={style.link}>
+                    <ListItem button divider >
                         <ListItemText primary="Profile"/>
                     </ListItem>
                 </NavLink>
-                <NavLink to={"/dialogs"} className={style.link} activeClassName={style.activeLink}>
+                <NavLink to={"/dialogs"} className={style.link}>
                     <ListItem button divider>
                         <ListItemText primary="Dialogs"/>
                     </ListItem>
                 </NavLink>
-                <NavLink to={"/users"} className={style.link} activeClassName={style.activeLink}>
+                <NavLink to={"/users"} className={style.link}>
                     <ListItem button divider>
-                        <ListItemText primary="Users" style={{textDecoration: "none"}}/>
+                        <ListItemText primary="Users"/>
                     </ListItem>
                 </NavLink>
-                <NavLink to={"/login"} className={style.link} activeClassName={style.activeLink}>
+                <NavLink to={"/login"} className={style.link}>
                     <ListItem button divider>
                         <ListItemText primary="Login"/>
                     </ListItem>
