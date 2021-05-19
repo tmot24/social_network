87f2e880bdda46b774fd01c91a44e4896e2b81d1
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-05-19 22:20:28 +0300

    chat more better

--- src/components/content/chat/Message.tsx
+++ src/components/content/chat/Message.tsx
@@ -1,16 +1,38 @@
 import React from "react"
 import {ChatMessageAPIType} from "../../../api/chat-api"
 import avatar from "../../../assets/images/avatar.svg"
+import {Avatar, Divider, Grid, Typography} from "@material-ui/core"
+import {makeStyles, useTheme} from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+    },
+    text: {
+        margin: theme.spacing(1),
+        flexGrow: 1
+    },
+    body: {
+        display: "flex",
+        justifyContent: "flex-end"
+    }
+}))
 
 export const Message: React.FC<{ message: ChatMessageAPIType }> = React.memo(({message}) => {
+    const theme = useTheme()
+    const classes = useStyles(theme)
 
     return (
-        <div>
-            <img alt={"img"} src={message.photo || avatar} style={{width: "30px"}}/>
-            <b>{message.userName}</b>
-            <br/>
-            {message.message}
-            <hr/>
-        </div>
+        <Grid container className={classes.root}>
+            <Grid item>
+                <Avatar alt={"img"} src={message.photo || avatar}/>
+            </Grid>
+            <Grid item className={classes.text}>
+                <Typography variant={"subtitle1"}>{message.userName}</Typography>
+                <Typography className={classes.body} variant={"body1"}>{message.message}</Typography>
+                <Divider variant={"fullWidth"}/>
+            </Grid>
+        </Grid>
     )
 })
--- src/components/content/chat/Messages.tsx
+++ src/components/content/chat/Messages.tsx
@@ -2,18 +2,30 @@ import {useSelector} from "react-redux"
 import {AppStateType} from "../../../redux/redux-store"
 import React, {useEffect, useRef} from "react"
 import {Message} from "./Message"
+import {Paper} from "@material-ui/core"
+import {makeStyles, useTheme} from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(1),
+        height: 400,
+        overflowY: "auto",
+    }
+}))
 
 export const Messages = () => {
     const messages = useSelector((state: AppStateType) => state.chat.messages)
     const messagesAnchorRef = useRef<HTMLDivElement>(null)
+    const theme = useTheme()
+    const classes = useStyles(theme)
 
     useEffect(() => {
         messagesAnchorRef.current?.scrollTo(0, 99999);
     }, [messages])
 
     return (
-        <div ref={messagesAnchorRef} style={{height: "400px", overflowY: "auto"}}>
+        <Paper className={classes.root} elevation={10} ref={messagesAnchorRef}>
             {messages.map((m, index) => <Message key={m.id} message={m}/>)}
-        </div>
+        </Paper>
     )
 }
